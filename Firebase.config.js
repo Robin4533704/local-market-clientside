a27961b1a7ff99225d2ed603fdf1cb0a76d97850
Robin4533704 <robinhossen8428@gmail.com>
2025-10-11 12:00:06 +0600

    save before Tailwind v4 upgrade

---
+++ Firebase.config.js
@@ -0,0 +1,24 @@
+// Firebase.config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // ✅ Firestore import
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication
+export const auth = getAuth(app);
+
+// Initialize Firestore and export it
+export const db = getFirestore(app); // ✅ Firestore export
+
