66fb2d610484182fec8d414454d6094e30295aad
Robin4533704 <robinhossen8428@gmail.com>
2025-10-13 15:05:14 +0600

    netlify deploy

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 
 const AuthLayout = () => {
   return (
-    <div className="p-12 bg-base-200 min-h-screen">
+    <div className="p-4 bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
    <div className='flex-1'>
      <img
